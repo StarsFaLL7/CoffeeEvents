7219b5a3fb7c4e393af70c0774467111bc011845
StarsFaLL7 <starsfall-ru@yandex.ru>
2025-01-11 20:11:29 +0500

    Feature: добавлены миграции и файл добавления всех сервисов из слоя Infrastructure

---
+++ CoffeeEvents/Infrastructure/InfrastructureStartup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureStartup
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContextFactory<ApplicationDbContext>();
+        return services;
+    }
+
+    public static void CheckAndMigrateDatabase(IServiceScope scope)
+    {
+        try
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.Migrate();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
