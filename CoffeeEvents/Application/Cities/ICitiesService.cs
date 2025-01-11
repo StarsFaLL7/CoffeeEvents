090f98b6295e6c84d7ff01d0d21278e35f8d0eb4
StarsFaLL7 <starsfall-ru@yandex.ru>
2025-01-11 20:14:35 +0500

    Feature: добавлен сервис для загрузки и получения списка городов

---
+++ CoffeeEvents/Application/Cities/ICitiesService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities.Cities;
+
+namespace Application.Cities;
+
+public interface ICitiesService
+{
+    public Task<FullCityModel[]> GetFullCitiesInfo(CityQuery queryParams);
+    
+    public Task<ShortCityModel[]> GetShortCitiesInfo(CityQuery queryParams);
+}
