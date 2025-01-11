d733fc6631c547bd23b9888db53da5e07d8afd07
StarsFaLL7 <starsfall-ru@yandex.ru>
2025-01-11 20:09:18 +0500

    Fix: обновлён dbContext

--- CoffeeEvents/Infrastructure/ApplicationDbContext.cs
+++ CoffeeEvents/Infrastructure/ApplicationDbContext.cs
@@ -1,31 +1,34 @@
-using Domain.Entities;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure;
 
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid, 
-    IdentityUserClaim<Guid>, IdentityUserRole<Guid>, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>
+public class ApplicationDbContext : DbContext
 {
-       private readonly string _connectionString;
+    private readonly string _connectionString;
     
     public DbSet<DynamicFieldType> DynamicFieldTypes { get; init; }
     public DbSet<EntryFieldValue> EntryFieldValues { get; init; }
     public DbSet<EventSignupEntry> EventSignupEntries { get; init; }
     public DbSet<EventSignupForm> EventSignupForms { get; init; }
+    public DbSet<EventSignupWindow> EventSignupWindows { get; init; }
     public DbSet<FormDynamicField> FormDynamicFields { get; init; }
     public DbSet<OrganizerContacts> OrganizerContacts { get; init; }
+    public DbSet<User> Users { get; init; }
     public DbSet<UserEvent> UsersEvents { get; init; }
+    public DbSet<UserRole> UsersRoles { get; init; }
+    public DbSet<RefreshToken> RefreshTokens { get; init; }
+    public DbSet<RevokedAccessToken> RevokedAccessTokens { get; init; }
     
     public ApplicationDbContext(IConfiguration configuration)
     {
-        const string connStringName = "DefaultConnection";
-        
-        var readedConnString = configuration.GetConnectionString(connStringName);
-        _connectionString = readedConnString ?? 
-                            throw new Exception($"Connection string \"{connStringName}\" wasn't found in configuration.");
+        var readedConnString = configuration.GetConnectionString("DefaultConnection");
+        if (readedConnString is null)
+        {
+            throw new Exception("Connection string \"DefaultConnection\" wasn't found in appsettings.json");
+        }
+        _connectionString = readedConnString;
     }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -33,9 +36,34 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
         optionsBuilder.UseSnakeCaseNamingConvention();
         optionsBuilder.UseNpgsql(_connectionString);
     }
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Добавление начальных данных
+        modelBuilder.Entity<UserRole>().HasData(
+            new UserRole
+            {
+                Id = Guid.NewGuid(),
+                Title = "User",
+                CanEditOthersEvents = false,
+                IsAdmin = false
+            },
+            new UserRole
+            {
+                Id = Guid.NewGuid(),
+                Title = "Admin",
+                CanEditOthersEvents = true,
+                IsAdmin = true
+            },
+            new UserRole
+            {
+                Id = Guid.NewGuid(),
+                Title = "Moderator",
+                CanEditOthersEvents = true,
+                IsAdmin = false
+            }
+        );
+        
         modelBuilder.Entity<DynamicFieldType>().HasData(
             new DynamicFieldType
             {
@@ -48,12 +76,5 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
                 Title = "number"
             }
         );
-        modelBuilder.Entity<IdentityUserLogin<Guid>>()
-            .HasKey(entity => entity.UserId);
-        modelBuilder.Entity<IdentityUserRole<Guid>>()
-            .HasKey(entity => new { entity.RoleId, entity.UserId } );
-        modelBuilder.Entity<IdentityUserToken<Guid>>()
-            .HasKey(entity => entity.UserId);
-        
     }
 }
